e0682c43f5d1db71c11f5ed918214598a01df071
stephen <statco19@gmail.com>
2021-02-10 10:59:57 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
-    path('<int:pk>/', views.single_post_page),  # Page not found(404) error
+    # path('<int:pk>/', views.single_post_page),  # Page not found(404) error
     # path('', views.index),
 ]
