9d4ee8787fdd8dee408e6a52014e46d8b39766fa
stephen <statco19@gmail.com>
2021-03-14 11:17:29 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
 from .models import Post
 
 # Create your tests here.
@@ -8,6 +9,10 @@ class TestView(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.user_trump = User.objects.create_user(username='trump',
+    password='somepassword')
+        self.user_obama = User.objects.create_user(username='obama',
+    password='somepassword')
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -52,10 +57,12 @@ class TestView(TestCase):
         post_001 = Post.objects.create(
             title = '첫 번째 포스트입니다.',
             content = 'Hello World. We are the world.',
+            author = self.user_trump,
         )
         post_002 = Post.objects.create(
             title = '두 번째 포스트입니다.',
             content = '1등이 전부는 아니잖아요?',
+            author = self.user_obama,
         )
         self.assertEqual(Post.objects.count(),2)
 
@@ -72,12 +79,16 @@ class TestView(TestCase):
         # 3.4 '아직 게시물이 없습니다'라는 문구는 더 이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
 
+        self.assertIn(self.user_trump.username.upper(), main_area.text)
+        self.assertIn(self.user_obama.username.upper(), main_area.text)
+
     def test_post_detail(self):
 
         # 1.1 포스트가 하나 있다.
         post_001 = Post.objects.create(
             title = '첫 번째 포스트입니다.',
             content = 'Hello World. We are the world.',
+            author = self.user_trump,
         )
 
         # 1.2 그 포스트의 url은 '/blog/1/'이다.
@@ -105,3 +116,5 @@ class TestView(TestCase):
 
         # 2.6 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
         self.assertIn(post_001.content, post_area.text)
+
+        self.assertIn(self.user_trump.username.upper(), main_area.text)
