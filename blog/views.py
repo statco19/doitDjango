bd5ee9133724a1bbb90a231b0aaad04588f8a6a6
stephen <statco19@gmail.com>
2021-03-20 13:24:40 +0900

    카테고리 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -26,16 +26,21 @@ class PostDetail(DetailView):
         return context
 
 def category_page(request, slug):
-    category = Category.objects.get(slug=slug)
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
 
     return render(
         request,
         'blog/post_list.html',
         {
-            'post_lsit': Post.objects.filter(category=category),
+            'post_list': post_list,
             'categories': Category.objects.all(),
             'no_category_post_count': Post.objects.filter(category=None).count(),
-            'category': Category,
+            'category': category,
         }
     )
 # def index(request):
